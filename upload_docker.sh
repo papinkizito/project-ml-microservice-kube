47c2f2ccde225f110b1ec3766a3ac2a30ff6a754
papinkizito <kizitop@outlook.com>
2023-06-08 01:27:13 +0000

    Update

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath="papinkizito/project-microservices"
 
 # Step 2:  
 # Authenticate & tag
+docker login --username=papinkizito
 echo "Docker ID and Image: $dockerpath"
+docker tag 3fe89267c24e $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker image push $dockerpath
